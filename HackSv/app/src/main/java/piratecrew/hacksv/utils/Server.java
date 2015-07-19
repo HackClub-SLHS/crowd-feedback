9e62155c614a57a59ad60cbe3c00b427ad219f02
xeonjake <jiahaokuang7066@gmail.com>
2015-07-18 18:07:46 -0700

    update

--- HackSv/app/src/main/java/piratecrew/hacksv/utils/Server.java
+++ HackSv/app/src/main/java/piratecrew/hacksv/utils/Server.java
@@ -1,6 +1,10 @@
 package piratecrew.hacksv.utils;
 
+import org.json.JSONArray;
+
+import java.io.BufferedOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import 	java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -12,19 +16,32 @@ public class Server {
 
     static private final String WEB_ROOT = "https://hacksv-server-xeonjake.c9.io/";
 
-    public String CreatePoll(String caption1, String caption2, String question, String email, String img1uri, String img2uri) throws IOException {
+    public String sendPoll( data) throws IOException {
         URL url = new URL(WEB_ROOT);
-        compileObject(caption1,caption2,question);
         HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        urlConnection.setReadTimeout(10000 /*milliseconds*/);
+        urlConnection.setConnectTimeout(15000 /* milliseconds */);
+        urlConnection.setRequestMethod("POST");
+        urlConnection.setDoInput(true);
+        urlConnection.setDoOutput(true);
+
+        //make some HTTP header nicety
+        urlConnection.setRequestProperty("Content-Type", "application/json;charset=utf-8");
+        urlConnection.setRequestProperty("X-Requested-With", "XMLHttpRequest");
+
+        //open connection
+        urlConnection.connect();
+
+        //setup send
+        OutputStream os = new BufferedOutputStream(urlConnection.getOutputStream());
+        os.write(data.getBytes());
+
+        //clean up
+        os.flush();
+        os.close();
+        urlConnection.disconnect();
+
         return null;
     }
-
-    public void compileObject(String caption1, String caption2, String question,) { //method to create a poll
-        String[] host = {WEB_ROOT + "create_poll.php"};
-        String[] cap1 = {"opt1", caption1};
-        String[] cap2 = {"opt2", caption2};
-        new sendPostRequest().execute(host, cap1, cap2, question);
-    }
-
 }
 
