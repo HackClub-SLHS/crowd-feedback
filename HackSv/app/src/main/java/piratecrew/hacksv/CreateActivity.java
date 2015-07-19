fcac8def2eb3e95eb8d849e609b89fa4e3b9fd3e
NSagan271 <naomi.sagan@gmail.com>
2015-07-18 21:04:46 -0700

    Create poll works
    Can retrieve bitmap
    Bitmap scaling added
    Connects to server (?)

--- HackSv/app/src/main/java/piratecrew/hacksv/CreateActivity.java
+++ HackSv/app/src/main/java/piratecrew/hacksv/CreateActivity.java
@@ -1,55 +1,61 @@
 package piratecrew.hacksv;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.lang.Runnable;
+import piratecrew.hacksv.util.Server;
 
-
-public class CreateActivity extends ActionBarActivity implements Runnable{
+public class CreateActivity extends AppCompatActivity implements Runnable{
 
     int requestCodeRun, resultCodeRun;
     Intent dataRun;
     Button create;
     EditText textTop, textCenter, textBottom;
-    Bitmap thumb, bit;
+    Bitmap bit1, bit2;
+    Bundle extras;
+    int imageToSet;
+    DisplayMetrics metrics;
+    int width;
+    int height;
+    Uri selectedImage;
+
 
     @Override
     public void run() {
-        super.onActivityResult(requestCodeRun, resultCodeRun, dataRun);
+        Bitmap bitmap = null;
         if (resultCodeRun == RESULT_OK) {
-            if (requestCodeRun == 1) {   //took a photo
-                File f = new File(Environment.getExternalStorageDirectory().toString());
-                for (File temp : f.listFiles()) {
-                    if (temp.getName().equals("temp.jpg")) {
-                        f = temp;
-                        break;
-                    }
-                }
-                try {
-                    Bitmap bitmap;
-                    BitmapFactory.Options bitmapOptions = new BitmapFactory.Options();
-                    bitmap = BitmapFactory.decodeFile(f.getAbsolutePath(),bitmapOptions);
-                    bit = bitmap;
-                    runOnUiThread(new Runnable(){
-                        @Override
-                        public void run() {
-                            ((ImageView) findViewById(imageToSet)).setImageBitmap(bit);
-                        }
-                    });
+            if (requestCodeRun ==1) {   //took a photo
+                bitmap = (Bitmap) extras.get("data");
+                    ((ImageView) findViewById(imageToSet)).setImageBitmap(Bitmap.createScaledBitmap(bitmap, (int)(width*((bitmap.getWidth()*height*.3)/(bitmap.getHeight()*width))), (int) (height*.3), false));
 
+                    //TODO: find why to use this code
+                   /* OutputStream outFile = null;
                     String path = android.os.Environment
                             .getExternalStorageDirectory()
-                            + File.separator
+                            +File.separator
                             + "createAct" + File.separator + "default"; //create the file name
-                    f.delete();
-                    OutputStream outFile = null;
                     File file = new File(path, String.valueOf(System.currentTimeMillis()) + ".jpg");
                     try {
                         outFile = new FileOutputStream(file);
-                        bitmap.compress(Bitmap.CompressFormat.JPEG, 85, outFile); //compress captured image
+                        bitmap.compress(Bitmap.CompressFormat.JPEG, 50, outFile); //compress captured image
                         outFile.flush();
                         outFile.close();
                     } catch (FileNotFoundException e) { //idk what these are it just had to be there
@@ -58,47 +64,60 @@ public class CreateActivity extends ActionBarActivity implements Runnable{
                         e.printStackTrace();
                     } catch (Exception e) {
                         e.printStackTrace();
-                    }
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
+                    }*/
 
             }
             else if (requestCodeRun == 2) {    //choose photo
 
-                Uri selectedImage = dataRun.getData();
-                String[] filePath = {MediaStore.Images.Media.DATA};
-                Cursor c = getContentResolver().query(selectedImage, filePath, null, null, null);
-                c.moveToFirst();
-                int columnIndex = c.getColumnIndex(filePath[0]);
-                String picturePath = c.getString(columnIndex);
-                c.close();
-                Bitmap thumbnail = (BitmapFactory.decodeFile(picturePath));
-                OutputStream compressThumbnail = null;
+                String[] filePathColumn = { MediaStore.Images.Media.DATA };
+                Cursor cursor = getContentResolver().query(selectedImage,filePathColumn, null, null, null);
+                cursor.moveToFirst();
+                int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                String picturePath = cursor.getString(columnIndex);
+                cursor.close();
+                bitmap = (BitmapFactory.decodeFile(picturePath));
+
+                //TODO: find why to use this code
+                /*OutputStream compressThumbnail = null;
                 try {
                     compressThumbnail = new FileOutputStream(picturePath);
                 } catch (FileNotFoundException e) {
                     e.printStackTrace();
                 }
                 thumbnail.compress(Bitmap.CompressFormat.JPEG,85,compressThumbnail); //use to compress the display image
-                Log.w("image path:", picturePath + "");
-                thumb = thumbnail;
-                runOnUiThread(new Runnable(){
-                    @Override
-                    public void run() {
-                        ((ImageView) findViewById(imageToSet)).setImageBitmap(thumb);
-                    }
-                });
+                Log.w("image path:", picturePath + "");*/
+                ((ImageView) findViewById(imageToSet)).setImageBitmap(Bitmap.createScaledBitmap(bitmap, (int)(width*((bitmap.getWidth()*height*.3)/(bitmap.getHeight()*width))), (int) (height*.3), false));
 
             }
         }
-        Log.i("Running", "Thread is running");
+        if (imageToSet == R.id.imageViewTop){
+            bit1 = bitmap;
+            bitmap.recycle();
+            selectImage(R.id.imageViewBottom, false);
+        }
+        else{
+            bit2 = bitmap;
+            bitmap.recycle();
+            Server.createPoll(textCenter.getText(),textTop.getText(),textBottom.getText(),bit1,bit2);
+        }
+         Log.i("Running", "Thread is running");
+    }
+
+    void showToast(CharSequence msg) {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create);
+
+        metrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay()
+                .getMetrics(metrics);
+        width = metrics.widthPixels;
+        height = metrics.heightPixels;
+
         textTop = (EditText) findViewById(R.id.editTextTop);
         textBottom = (EditText) findViewById(R.id.editTextBottom);
         textCenter = (EditText) findViewById(R.id.editTextCenter);
@@ -109,20 +128,11 @@ public class CreateActivity extends ActionBarActivity implements Runnable{
                 //Turn the time into milliseconds
                 long time = 60_000 * (10 + 60 * (0 + 24 * 0));
 
-                for(byte i = 0;i<=1;i++) {
-                    //TODO: add code to verify inputs and upload to server
-
-                    //TODO add additional verification for text boxes
-                    if (textTop.getText().toString().equals("") || textBottom.getText().toString().equals("")|| textCenter.getText().toString().equals("")) {
-                        showToast("Must describe both options and add question.");
-                        break;
-                    }
-                    else {
-                        selectImage(R.id.imageViewTop);
-                        selectImage(R.id.imageViewBottom);
-                        //TODO: Database code for creating poll
-                    }
-
+                //TODO add additional verification for text boxes
+                if (textTop.getText().toString().equals("") || textBottom.getText().toString().equals("") || textCenter.getText().toString().equals("")) {
+                    showToast("Must describe both options and add question.");
+                } else {
+                    selectImage(R.id.imageViewTop, true);
 
                 }
             }
@@ -130,31 +140,34 @@ public class CreateActivity extends ActionBarActivity implements Runnable{
     }
 
 
-    int imageToSet;
-    private void selectImage(int im) { // select image button
+    private void selectImage(int im, boolean first) { // select image button
         //im is the right image id when the right button is pressed.
         //im is the left  image id when the left  button is pressed.
         imageToSet = im;
 
         //Build prompt to take a photo
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle(R.string.photo_dialog_title);
-        builder.setItems(R.array.photo_options, new DialogInterface.OnClickListener() {
+        if (first) builder.setTitle("Choose your first photo:");
+        else builder.setTitle("Choose your second photo:");
+        final CharSequence[] photoOptions = {"Take a photo", "Choose from gallery", "Cancel"};
+        builder.setItems(photoOptions, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int item) {
                 Intent intent;
                 switch (item) {
                     //Take a photo
                     case 0:
-                        intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                        File f = new File(android.os.Environment.getExternalStorageDirectory(), "temp.jpg");
-                        intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(f));
+                         intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
                         startActivityForResult(intent, 1);
+                        resultCodeRun = RESULT_OK;
+                        requestCodeRun = 1;
                         break;
                     //Chose a photo from gallery
                     case 1:
-                        intent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                        startActivityForResult(intent, 2);
+                        Intent i = new Intent(Intent.ACTION_PICK,android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                        resultCodeRun = RESULT_OK;
+                        requestCodeRun = 2;
+                        startActivityForResult(i, 2);
                         break;
                     case 2:
                         dialog.cancel();
@@ -171,7 +184,17 @@ public class CreateActivity extends ActionBarActivity implements Runnable{
         return true;
     }
 
-
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+            extras =data.getExtras();
+            run();
+        }
+        else if (requestCode == 2 && resultCode == RESULT_OK){
+            selectedImage = data.getData();
+            run();
+        }
+    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
